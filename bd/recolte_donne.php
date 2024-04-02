c46904cc2b101b21c60c4b701289c826e989c196
wicra SERGIO <w.sergio@P210.sn>
2024-04-02 16:55:00 +0200

    Maj tout fonctionnelle

--- bd/recolte_donne.php
+++ bd/recolte_donne.php
@@ -18,25 +18,23 @@
     echo "Connexion à la base de données réussie !";
 
     // Boucle pour générer des données simulées en continu
-    while (true) {
-        // Génération de valeurs aléatoires pour la température et l'humidité
-        $temperature = rand(23, 29);
-        $humidite = rand(12, 19);
 
-        // Commande SQL pour insérer les données simulées dans la table "mesure"
-        $sql = "INSERT INTO mesure (temperature, humidite) VALUES ($temperature, $humidite)";
+    // Génération de valeurs aléatoires pour la température et l'humidité
+    $temperature =$_GET['temperature'];
+    $humidite = $_GET['humidite'];
 
-        // Exécution de la commande SQL
-        if (mysqli_query($conn, $sql)) {
-            //Nouvelle valeur enregistrée 
-            echo "T= $temperature, H = $humidite<br>";
-        } else {
-            echo "Erreur lors de l'insertion des données : " . mysqli_error($conn);
-        }
+    // Commande SQL pour insérer les données simulées dans la table "mesure"
+    $sql = "INSERT INTO mesure (temperature, humidite) VALUES ($temperature, $humidite)";
 
-        // Attente de 2 secondes avant de générer de nouvelles données
-        sleep(3);
+    // Exécution de la commande SQL
+    if (mysqli_query($conn, $sql)) {
+        //Nouvelle valeur enregistrée 
+        echo "T= $temperature, H = $humidite<br>";
+    } else {
+        echo "Erreur lors de l'insertion des données : " . mysqli_error($conn);
     }
+
+
 
     // Fermeture de la connexion à la base de données (ce code ne sera jamais atteint dans une boucle infinie)
     mysqli_close($conn);
